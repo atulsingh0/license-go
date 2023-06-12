ceb159cdc22ea156143376349d54cd846985e4c8
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-06-12 06:24:23 -0400

    main | added controllers

---
+++ src/controllers/PostGenerate.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/atulsingh0/license-go/src/generate"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PostGenerate(c *gin.Context) {
+
+	// reading and binding the post data
+	inpBody := &generate.Rlic{}
+	c.ShouldBindJSON(&inpBody)
+
+	// Validating the input data
+	err := inpBody.InputValidation()
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusPartialContent, gin.H{
+			"error": err.Error(),
+			"code":  http.StatusPartialContent,
+		})
+	} else {
+
+		//generating the license
+		lic, err := inpBody.Generate()
+
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"error": err.Error(),
+				"code":  http.StatusUnprocessableEntity,
+			})
+		} else {
+			c.JSON(http.StatusOK, lic)
+		}
+
+	}
+
+}
