70991405d92f6ca02a08119fa6495f99a023cfa3
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-06-11 20:48:57 -0400

    main | renamed json field

--- src/models/input.go
+++ src/models/input.go
@@ -3,7 +3,7 @@ package models
 type RawLicense struct {
 	Version    string `json:"version"`
 	Customer   string `json:"customer"`
-	ValidFrom  string `json:"validfrom"`
+	ValidFrom  string `json:"valid-from"`
 	Expiry     string `json:"expiry-date"`
 	HardExpiry string `json:"hard-expiry-date"`
 	Seats      int    `json:"seats"`
