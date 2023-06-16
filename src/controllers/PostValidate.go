ed71ea6e3dfb282007f221c10ca3a8affca1ddec
atulsingh0 <3430610+atulsingh0@users.noreply.github.com>
2023-06-16 02:53:50 -0400

    main | added validation controls

--- src/controllers/PostValidate.go
+++ src/controllers/PostValidate.go
@@ -3,13 +3,27 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/atulsingh0/license-go/src/validate"
 	"github.com/gin-gonic/gin"
 )
 
 func PostValidate(c *gin.Context) {
 
-	// TODO:
-	c.JSON(http.StatusOK, gin.H{
-		"msg": "work to do",
-	})
+	// reading and binding the post data
+	inpBody := validate.Slic{}
+	c.ShouldBindJSON(&inpBody)
+
+	// Validating the input License
+	err := inpBody.Validate()
+
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{
+			"error": err.Error(),
+			"code":  http.StatusForbidden,
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "License is valid.",
+		})
+	}
 }
